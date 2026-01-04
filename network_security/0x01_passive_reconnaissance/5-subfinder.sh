6918063af5de052255d43244a61f05aa87cc44a3
cayciahmet <nbiyevashla215@gmail.com>
2026-01-04 17:32:04 +0000

    Fix: Use exact subfinder flags required by checker

--- network_security/0x01_passive_reconnaissance/5-subfinder.sh
+++ network_security/0x01_passive_reconnaissance/5-subfinder.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-subfinder -d "$1" -silent -all | xargs -I {} sh -c 'ip=$(dig +short {} | grep -E "^[0-9.]+$" | head -n1); if [ ! -z "$ip" ]; then echo "{},$ip"; fi' > "$1.txt"
+subfinder -silent -d "$1" -o "$1.txt" -nW -oI
