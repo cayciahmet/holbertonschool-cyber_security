eb2e2707259f36fead881ee52d712f417244b71e
cayciahmet <nbiyevashla215@gmail.com>
2026-03-03 15:56:58 -0500

    Task 4: Update script with improved awk logic and head limit

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --wordlist=/usr/share/wordlists/rockyou.txt $1 && john --show $1 | awk -F: "NR>1 {print \$2}" > 4-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt "$1" && john --show "$1" | awk -F: "NR>1 && NF>1 {print \$2}" | head -n 3 > 4-password.txt
