c4160d7dbcc15c1b0021a3768b919353bae70de8
cayciahmet <nbiyevashla215@gmail.com>
2026-03-03 16:03:33 -0500

    Task 4: Final universal script with robust awk filtering

--- cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
+++ cybersecurity_basics/0x03_cryptography_basics/4-wordlist_john.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-john --format=Raw-MD5 --wordlist=/usr/share/wordlists/rockyou.txt "$1" && john --show --format=Raw-MD5 "$1" | cut -d: -f2 | grep -v "password hashes" > 4-password.txt
+john --wordlist=/usr/share/wordlists/rockyou.txt "$1" && john --show "$1" | awk -F: 'NF>1 {print $2}' > 4-password.txt
