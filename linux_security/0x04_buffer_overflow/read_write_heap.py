6838c6b7f29946d797ed4d71d52e9e0c2d20d0fa
cayciahmet <nbiyevashla215@gmail.com>
2026-04-06 18:41:42 -0400

    Fix: Minimal version strictly aligned with Holberton Checker

--- linux_security/0x04_buffer_overflow/read_write_heap.py
+++ linux_security/0x04_buffer_overflow/read_write_heap.py
@@ -1,63 +1,45 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
-Finds a string in the heap of a running process and replaces it.
+Locates and replaces the first occurrence of a string in the heap
+of a running process.
 """
 import sys
 
 
-def print_usage_and_exit():
-    """Prints usage message to stdout and exits with status 1."""
-    print("Usage: read_write_heap.py pid search_string replace_string")
-    sys.exit(1)
-
-
 def main():
-    """Main entry point for the script."""
+    """Main function to find and replace string in heap."""
     if len(sys.argv) != 4:
-        print_usage_and_exit()
+        print("Usage: read_write_heap.py pid search_string replace_string")
+        sys.exit(1)
 
     pid = sys.argv[1]
-    search_string = sys.argv[2]
-    replace_string = sys.argv[3]
+    search_str = sys.argv[2]
+    replace_str = sys.argv[3]
+    start = None
+    end = None
 
-    maps_path = "/proc/{}/maps".format(pid)
-    mem_path = "/proc/{}/mem".format(pid)
-
-    start_addr = None
-    end_addr = None
-
-    # Find heap boundaries
     try:
-        with open(maps_path, "r") as maps_file:
-            for line in maps_file:
+        with open("/proc/{}/maps".format(pid), "r") as maps:
+            for line in maps:
                 if "[heap]" in line:
-                    addr_range = line.split()[0]
-                    start_str, end_str = addr_range.split("-")
-                    start_addr = int(start_str, 16)
-                    end_addr = int(end_str, 16)
+                    start_str, end_str = line.split()[0].split("-")
+                    start = int(start_str, 16)
+                    end = int(end_str, 16)
                     break
     except Exception:
         sys.exit(1)
 
-    if start_addr is None:
+    if start is None:
         sys.exit(1)
 
-    # Read, find and replace in memory
     try:
-        with open(mem_path, "r+b") as mem_file:
-            mem_file.seek(start_addr)
-            heap_data = mem_file.read(end_addr - start_addr)
-
-            search_bytes = search_string.encode("ascii")
-            # C strings are null-terminated
-            replace_bytes = replace_string.encode("ascii") + b'\x00'
-
-            offset = heap_data.find(search_bytes)
-            if offset == -1:
-                sys.exit(1)
-
-            mem_file.seek(start_addr + offset)
-            mem_file.write(replace_bytes)
+        with open("/proc/{}/mem".format(pid), "r+b") as mem:
+            mem.seek(start)
+            heap = mem.read(end - start)
+            index = heap.find(search_str.encode('ascii'))
+            if index != -1:
+                mem.seek(start + index)
+                mem.write(replace_str.encode('ascii') + b'\x00')
     except Exception:
         sys.exit(1)
 
